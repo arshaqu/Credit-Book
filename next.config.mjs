09be7ca5de7d2235840c5f01d9a0dc48435151a0
Arshaq <muhammedarshaque1999@gmail.com>
2026-03-05 10:34:24 +0400

    Use webpack for PWA support

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-};
+import withPWA from '@ducanh2912/next-pwa';
+
+const nextConfig = withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+})({
+  reactStrictMode: true,
+  turbopack: {}, // silences the turbopack warning
+});
 
 export default nextConfig;
